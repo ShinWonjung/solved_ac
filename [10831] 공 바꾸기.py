dbb70111bb84a756e560530b3c347cb3b0f4bd23
ShinWonjung <dnjswjd1204@naver.com>
2026-03-27 19:49:52 +0900

    10813

---
+++ [10831] 공 바꾸기.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr = []
+for i in range(n):
+    arr.append(i+1)
+
+for i in range(m):
+    a, b = map(int, input().split())
+    a1 = arr[a-1]
+    b1 = arr[b-1]
+    arr[a-1] = b1
+    arr[b-1] = a1
+
+for i in arr:
+    print(i,end=' ')
